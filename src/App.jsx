b598fa67fc82d882fbf65fc1578578cbac0ba33d
Ayodele Owolabi <joyowolabi@gmail.com>
2024-11-18 20:27:10 +0100

    hover functionlity and tabs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,27 @@ import Experience from "./components/Experience.jsx";
 import Technologies from "./components/Technologies.jsx";
 import Project from "./components/Project.jsx"
 function App() {
+  let component 
+  switch (window.location.pathname) {
+    case "/hero":
+      component =  <Hero />
+      break
+  }
+  switch (window.location.pathname) {
+    case "/about":
+      component =  <About />
+      break
+  }
+  switch (window.location.pathname) {
+    case "/project":
+      component =  <Project />
+      break
+  }
+  switch (window.location.pathname) {
+    case "/experience":
+      component =  <Experience />
+      break
+  }
   return (
     <div className="overflow-x-hidden text-neutral-500 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
@@ -13,6 +34,7 @@ function App() {
       </div>
      
         <Navbar />
+        <div className="container">{component}</div>
         <Hero />
         <Technologies/>
         <Project/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,19 +5,25 @@ import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
     return <nav className=" bg-slate-950 mb-10 flex items-center justify-between py-1">
         <div className="flex flex-shrink-0 items-center m-4">
-            <h1 className="text-slate-50">AO</h1>
+            <h1 className="text-slate-50 hover:bg-neutral-400 rounded-sm text-2xl"> <a href="/hero"> AO </a></h1>
+            <li>
+                <a href="/about" className="p-3 hover:bg-slate-100 rounded-sm">About</a>
+                <a href="/project" className="p-3 hover:bg-slate-100 rounded-sm">Projects</a>
+                <a href="/experience" className="p-3 hover:bg-slate-100 rounded-sm">Experience</a>
+                
+            </li>
         </div>
         <img src="" alt="" />
         <div className="m-10 flex items-center justify-center gap-4 text-2xl">
-            <a href="https://www.linkedin.com/in/ayoowolabi/">
+            <a href="https://www.linkedin.com/in/ayoowolabi/" className="hover:bg-slate-100 rounded-sm">
         <FaLinkedin />
             </a>
-            <a href="https://github.com/ayodeleowolabi">
+            <a href="https://github.com/ayodeleowolabi" className="hover:bg-slate-100 rounded-sm">
 
         <FaGithub />
 
             </a>
-            <a href="https://www.instagram.com/iothesinger/">
+            <a href="https://www.instagram.com/iothesinger/" className="hover:bg-slate-100 rounded-sm">
 
 
         <FaInstagram />
